1366ba2d49dae65dcf0a4a744f66a70d5ae4f618
theprogrammer67 <igor@stovpets.com>
2025-12-12 21:35:25 +0300

    change the names of SQL queries

--- sqlset_test.go
+++ sqlset_test.go
@@ -42,19 +42,19 @@ func TestSQLSet(t *testing.T) {
 	}{
 		{
 			setID:         "test-id-override-1",
-			queryID:       "notification",
+			queryID:       "GetData1",
 			expected:      "SELECT '515bbf3c-93c5-476a-8dbc-4a6db4fe3c0c' AS id, 'Igor' AS name, 'en' AS language, 'igor@example.com' AS email, ARRAY['token1','token2'] AS tokens;",
 			expectedFound: true,
 		},
 		{
 			setID:         "test-id-override-1",
-			queryID:       "email",
+			queryID:       "GetData2",
 			expected:      "SELECT 'ef84af8f-bb55-4f74-9d7c-3db30e740d20' AS id, 'Alexey' AS name, 'en' AS language, 'alex@example.com' AS email, '{}'::varchar[] as tokens;",
 			expectedFound: true,
 		},
 		{
 			setID:         "test-id-override-1",
-			queryID:       "both",
+			queryID:       "GetData3",
 			expected:      "SELECT 'e192f9e5-5e5c-4bba-b13e-0f9de32ec6bd' AS id, 'Denis' AS name, 'en' AS language, 'denis@example.com' AS email, ARRAY['token3','token4'] AS tokens;",
 			expectedFound: true,
 		},
